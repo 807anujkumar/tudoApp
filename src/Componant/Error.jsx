7438e501616bf37570f0882722e74121efcc2f2a
807anujkumar <807anuj@gmail.com>
2024-01-17 09:40:20 +0530

    add css in heading

--- src/Componant/Error.jsx
+++ src/Componant/Error.jsx
@@ -1,7 +1,12 @@
  export default function Error()
 {
+    const myComponentStyle = {  
+        color: 'darkblue', 
+        FontFace:8, 
+        margin:20,
+    }  
     return<>
- <h4>Please Enter the items</h4>
+ <h4 style={myComponentStyle}>Please Enter the items</h4>
 
     </>
 }
