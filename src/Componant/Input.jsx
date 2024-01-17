e72d8f73e0b3211f88052d363c33b2c3d637e828
807anujkumar <807anuj@gmail.com>
2024-01-17 09:21:52 +0530

    css change

--- src/Componant/Input.jsx
+++ src/Componant/Input.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react"
 import style from "./Input.module.css"
+
 export function Input({onclickadd}){
 
 const[inputvalue,setinputvalue]=useState("")
 const[inputvalue1,setinputvalue1]=useState("")
+
 function  inputhandler(event) {
   setinputvalue(event.target.value);
 }
@@ -28,9 +30,8 @@ else
 
  }
 return<>
- 
-<div className={`row g-3 ${style.mainContainer}`}>
-      <div className={`col`}>
+ <div className={`row g-3 ,${style.mainContainer}`}>
+      <div className={`col ${style.Inputdiv} `}>
         <input type="text" className={`form-control ${style.input1}`} placeholder="Enter the Item"onChange={inputhandler} value={inputvalue} required/>
       </div>
 
@@ -38,10 +39,9 @@ return<>
         <input type="date" className={`form-control ${style.date1}`} onChange={inputhandlerdate} value={inputvalue1}/>
       </div>
       <div className={`col ${style.btm}`}>
-      <button type="submit" className={`btn btn-primary ${style.btm1}`} onClick={addhandler}>Add</button>
+      <button type="submit" className={`btn btn-primary $  {style.btm1}`} onClick={addhandler}>Add</button>
     </div>
-
-    </div>
+</div>
     </>
 
  }
